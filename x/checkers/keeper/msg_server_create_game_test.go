e7b4920d50e9137c00d85747726b05e56ac0deb9
Peppe Landolfi <giuseppe.landolfi@gmail.com>
2023-05-22 11:57:01 +0200

    Add CreateGame msg

---
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -0,0 +1,20 @@
+package keeper_test
+
+import (
+	"github.com/peppelan/checkers/x/checkers/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestCreateGame(t *testing.T) {
+	msgServer, context := setupMsgServer(t)
+	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		GameIndex: "", // TODO: update with a proper value when updated
+	}, *createResponse)
+}
